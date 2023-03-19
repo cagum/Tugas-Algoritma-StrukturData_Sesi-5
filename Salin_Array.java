a93e9b8346e8cdaebb7fb2262872c6d8986edb2d
cagum <123584871+cagum@users.noreply.github.com>
2023-03-19 19:11:58 +0700

    Tugas Sesi-5_Agoritma_MochCahyaG

---
+++ Salin_Array.java
@@ -0,0 +1,28 @@
+public class array {
+    
+    public static void main(String[] args) {
+        int bilangan[] = new int[3];
+        bilangan[0] = 1;
+        bilangan[1] = 2;
+        bilangan[2] = 3;
+        for (int i = 0; i < bilangan. length; i++) {
+            System.out.print(bilangan[i]+" ");
+    
+        }
+        System.out.println();
+        int bil[] = {1,5,7,9,11,14,16,18,20};
+    
+        for (int i = 0; i < bil.length; i++) {
+            System.out.print(bil[i]+" ");
+        }
+        System.out.println();
+        String[] mahasiswa = {"Rizki", "Rahmat", "Rahman", "Rahmi"};
+    
+        for (int i=0; i < mahasiswa.length; i++) {
+            System.out.print(mahasiswa[i]+" ");
+
+        }
+    }
+
+}    
+    
